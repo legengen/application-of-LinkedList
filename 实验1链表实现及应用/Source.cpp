cb1fa15d200a275d62b9adc2e3f48defdaa52e3d
legengen <1991171956@qq.com>
2024-09-25 12:10:20 +0800

    add createLinkList

---
+++ 实验1链表实现及应用/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+typedef int ElemType;
+struct Node {
+	ElemType data;
+	Node* next;
+};
+typedef Node* LinkList;
+LinkList createLinkList();
+int main() {
+
+}
+LinkList createLinkList() {
+	LinkList head = nullptr;
+	LinkList newnode = new Node;
+	
+}
